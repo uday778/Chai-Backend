3910a21611acb405c5aaa33a45acdde8116d0753
uday kumar <uday33379@gmail.com>
2024-01-25 15:03:14 +0530

    added db connection

---
+++ db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../src/constants.js";
+
+const dbconnect=async()=>{
+    try{
+       const connectionInstances= await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+       console.log(`\n mongodb connected !! db host:${connectionInstances.connection.host}`);
+    }catch(err){
+        console.log(" Mongo db connection failed",err)
+        process.exit(1)
+    }
+}
+
+export default dbconnect
